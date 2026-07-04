8fee953c502b8090f6a35c10af9fbc6067b71211
Linnea Gräf <nea@nea.moe>
2026-07-04 02:13:04 +0200

    small little patch check in

--- packages/ecj/package.nix
+++ packages/ecj/package.nix
@@ -27,50 +27,6 @@ let
     ]
     ++ lib.optionals finalAttrs.withJikes [ jikes ]
     ++ lib.optionals (finalAttrs.previousEcj != null) [ finalAttrs.previousEcj ];
-    srcJdt = fetchFromGitHub {
-      name = "eclipse-jdt";
-      owner = "eclipse-jdt";
-      repo = "eclipse.jdt.core";
-      rev = "f3fb33fde9b5fd8fd3021a7432912bb6fbeb17c7"; # JDK_1_5
-      hash = "sha256-X6ACLma4sbBLNwi221CV3hG7lDIHD9FaArGK7ybWQD0=";
-    };
-    srcs = [
-      finalAttrs.srcJdt
-      (fetchFromGitHub {
-        name = "eclipse-platform-resources";
-        owner = "eclipse-platform";
-        repo = "eclipse.platform.resources";
-        rev = "v20040624a";
-        hash = "sha256-TkyFY6QcPGbbBs+F+sAlg+DfIHgI/kWo68Glo7H9L9c=";
-      })
-      (fetchFromGitHub {
-        name = "eclipse-platform-runtime";
-        owner = "eclipse-platform";
-        repo = "eclipse.platform.runtime";
-        rev = "v20040625_1200";
-        hash = "sha256-pVZjBZbvMtK3/AYGjOfjBDIRvPe18GKj8yWLRFcZVAE=";
-      })
-      #    (fetchFromGitHub {
-      #      owner = "osgi";
-      #      repo = "osgi";
-      #      rev = "01111010ac6c558ca5f169afec32a39847858110";
-      #      hash = "sha256-S+BDCRcie8gG4nzsGHo3Cx/orY9QP2Gw9XgdYD/H6/Y=";
-      #    })
-      (fetchFromGitHub {
-        name = "eclipse-equinox";
-        owner = "eclipse-equinox";
-        repo = "equinox";
-        rev = "c6557fef063db7fbf02744824849517ba641c56a";
-        hash = "sha256-Q7VhfAZGsDSW5imKGgQoIMHZFbFyGITn1oJbYoMhqCg=";
-      })
-      (fetchFromGitHub {
-        name = "eclipse-platform-text";
-        owner = "eclipse-platform";
-        repo = "eclipse.platform.text";
-        rev = "v20040625_1200";
-        hash = "sha256-kHpLTc2dwfIq37tO/FgmLQJLcG1YlDUsrNhztRuLEzA=";
-      })
-    ];
     sourceRoot = "ecj-source";
     zippedSourceProjects = [
       "org.eclipse.osgi.services"
@@ -149,6 +105,49 @@ let
         patches = [
           ./ecj.patch
         ];
+        srcs = [
+          (fetchFromGitHub {
+            name = "eclipse-jdt";
+            owner = "eclipse-jdt";
+            repo = "eclipse.jdt.core";
+            rev = "f3fb33fde9b5fd8fd3021a7432912bb6fbeb17c7"; # JDK_1_5
+            hash = "sha256-X6ACLma4sbBLNwi221CV3hG7lDIHD9FaArGK7ybWQD0=";
+          })
+          (fetchFromGitHub {
+            name = "eclipse-platform-resources";
+            owner = "eclipse-platform";
+            repo = "eclipse.platform.resources";
+            rev = "v20040624a";
+            hash = "sha256-TkyFY6QcPGbbBs+F+sAlg+DfIHgI/kWo68Glo7H9L9c=";
+          })
+          (fetchFromGitHub {
+            name = "eclipse-platform-runtime";
+            owner = "eclipse-platform";
+            repo = "eclipse.platform.runtime";
+            rev = "v20040625_1200";
+            hash = "sha256-pVZjBZbvMtK3/AYGjOfjBDIRvPe18GKj8yWLRFcZVAE=";
+          })
+          #    (fetchFromGitHub {
+          #      owner = "osgi";
+          #      repo = "osgi";
+          #      rev = "01111010ac6c558ca5f169afec32a39847858110";
+          #      hash = "sha256-S+BDCRcie8gG4nzsGHo3Cx/orY9QP2Gw9XgdYD/H6/Y=";
+          #    })
+          (fetchFromGitHub {
+            name = "eclipse-equinox";
+            owner = "eclipse-equinox";
+            repo = "equinox";
+            rev = "c6557fef063db7fbf02744824849517ba641c56a";
+            hash = "sha256-Q7VhfAZGsDSW5imKGgQoIMHZFbFyGITn1oJbYoMhqCg=";
+          })
+          (fetchFromGitHub {
+            name = "eclipse-platform-text";
+            owner = "eclipse-platform";
+            repo = "eclipse.platform.text";
+            rev = "v20040625_1200";
+            hash = "sha256-kHpLTc2dwfIq37tO/FgmLQJLcG1YlDUsrNhztRuLEzA=";
+          })
+        ];
         withJikes = true;
       }
     );
@@ -159,13 +158,76 @@ let
         patches = [
           ./ecj33.patch
         ];
-        srcJdt = fetchFromGitHub {
-          name = "eclipse-jdt";
-          owner = "eclipse-jdt";
-          repo = "eclipse.jdt.core";
-          rev = "R3_3";
-          hash = "sha256-6bbi/IqI8LaW0CfF6nTDTZDD56zWNSGV6b4PBVyCtac=";
-        };
+        srcs =
+          let
+            rev = "R3_3";
+          in
+          [
+            (fetchFromGitHub {
+              name = "eclipse-jdt";
+              owner = "eclipse-jdt";
+              repo = "eclipse.jdt.core";
+              inherit rev;
+              hash = "sha256-6bbi/IqI8LaW0CfF6nTDTZDD56zWNSGV6b4PBVyCtac=";
+            })
+            (fetchFromGitHub {
+              name = "eclipse-platform-resources";
+              owner = "eclipse-platform";
+              repo = "eclipse.platform.resources";
+              inherit rev;
+              hash = "sha256-7WfdxG78gEKOR+78zDRJZIGo6Oi+AXA9Rb/raOXm4xg=";
+            })
+            (fetchFromGitHub {
+              name = "eclipse.platform.ui";
+              owner = "eclipse-platform";
+              repo = "eclipse.platform.ui";
+              inherit rev;
+              hash = "sha256-lsz83IJ8ewS+LM5+YOdjNNl9/iaoPW19mr6G1aVUZrY=";
+            })
+            (fetchFromGitHub {
+              name = "eclipse-platform-runtime";
+              owner = "eclipse-platform";
+              repo = "eclipse.platform.runtime";
+              inherit rev;
+              hash = "sha256-lCWAHQtIulC6w4zUn6QPwjKu1BqaZ4uwhfoLWivxMuc=";
+            })
+            (fetchFromGitHub {
+              name = "equinox.bundles";
+              owner = "eclipse-equinox";
+              repo = "equinox.bundles";
+              inherit rev;
+              hash = "sha256-fM0KR7oUadOit0xQjZjnYKuwk9jffSk/87FvJOTev94=";
+            })
+            (fetchFromGitHub {
+              name = "equinox.framework";
+              owner = "eclipse-equinox";
+              repo = "equinox.framework";
+              inherit rev;
+              hash = "sha256-C6/obyyzaYU/0EmJMIIk9oKpTjgupPYqQzQu37TP8A0=";
+            })
+            #            (fetchFromGitHub {
+            #              name = "eclipse-equinox";
+            #              owner = "eclipse-equinox";
+            #              repo = "equinox";
+            #              rev = "0ff9893cd43bace07b119af983f3c4ac74b98194";
+            #              hash = "sha256-nI7SCNh5RQi2TCXx1XmZDWKy8+tyT927W8Tg+y2BbFA=";
+            #            })
+            (fetchFromGitHub {
+              name = "eclipse-platform-text";
+              owner = "eclipse-platform";
+              repo = "eclipse.platform.text";
+              inherit rev;
+              hash = "sha256-E3471tnuBqrJ8CwNK79t0gIptfRrYN9TjsvHDovk09U=";
+            })
+          ];
+        #      2 EventDispatcher.java
+        #      2 EventListeners.java
+        #      2 EventManager.java
+        #      2 ListenerQueue.java
+        #      2 URLStreamHandlerSetter.java
+        #      3 Constants.java
+        #      3 Handler.java
+        zippedSourceProjects = [ ];
         passthru.tests = {
           "ecj-1.6" = makeE2E {
             languageVersion = "1.6";
@@ -175,24 +237,24 @@ let
         };
       }
     );
-    ecj383 = ecjBare.overrideAttrs (
-      # this is me being dumb and building an _older_ ecj version
-      final: prev: {
-        previousEcj = ecj501;
-        version = "0.383";
-        patches = [
-          ./ecj2.patch
-        ];
-        javaVersion = "1.5";
-        srcJdt = fetchFromGitHub {
-          name = "eclipse-jdt";
-          owner = "eclipse-jdt";
-          repo = "eclipse.jdt.core";
-          rev = "v_382a";
-          hash = "sha256-Kdes4eso3DIfDjblJ60nfg+Vyj9Gmh7F7AkHQqDcex0=";
-        };
-      }
-    );
+    #    ecj383 = ecjBare.overrideAttrs (
+    #      # this is me being dumb and building an _older_ ecj version
+    #      final: prev: {
+    #        previousEcj = ecj501;
+    #        version = "0.383";
+    #        patches = [
+    #          ./ecj2.patch
+    #        ];
+    #        javaVersion = "1.5";
+    #        srcJdt = fetchFromGitHub {
+    #          name = "eclipse-jdt";
+    #          owner = "eclipse-jdt";
+    #          repo = "eclipse.jdt.core";
+    #          rev = "v_382a";
+    #          hash = "sha256-Kdes4eso3DIfDjblJ60nfg+Vyj9Gmh7F7AkHQqDcex0=";
+    #        };
+    #      }
+    #    );
     latest = ecjR3_3;
   };
 in
