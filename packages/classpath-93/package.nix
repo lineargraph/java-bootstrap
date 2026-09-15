b19da35bf976108bebdd5c6c3b14d768f66db912
Linnea Gräf <nea@nea.moe>
2026-09-15 19:40:13 +0200

    classpath rework

--- packages/classpath-93/package.nix
+++ packages/classpath-93/package.nix
@@ -1,12 +1,44 @@
 {
-  callPackage,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  findutils,
+  glib,
+  zip,
+  lib,
+  jikes,
 }:
-(callPackage ./base.nix { hash = "sha256-3y0JNhKr0j/mfpQJ2JuyqOebFmT+Ky2kDhyO1pPjKUU="; })
-.overrideAttrs
-  (
-    final: prev: {
-      withJikes = true;
-      version = "0.93";
-      patches = [ ./classpath93.patch ];
-    }
-  )
+stdenv.mkDerivation (finalAttrs: {
+  version = "0.93";
+  patches = [ ./classpath93.patch ];
+  pname = "classpath";
+  buildInputs = [
+    glib
+  ];
+  nativeBuildInputs = [
+    pkg-config
+    findutils
+    zip
+    jikes
+  ];
+  configureFlags = [
+    "--with-jikes"
+    "--disable-plugin"
+    "--disable-gtk-peer"
+    "--enable-default-preferences-peer=memory"
+    "--disable-gconf-peer"
+    # "--disable-examples"
+    # "--enable-Werror"
+  ];
+  # postInstall = ''
+  #   mkdir -p $out/nix-support
+  #   echo "export BOOTCLASSPATH=$out/share/classpath/glibj.zip" > $out/nix-support/setup-hook
+  # '';
+  src = fetchurl {
+    url = "mirror://gnu/classpath/classpath-${finalAttrs.version}.tar.gz";
+    hash = "sha256-3y0JNhKr0j/mfpQJ2JuyqOebFmT+Ky2kDhyO1pPjKUU=";
+  };
+  meta = {
+    license = lib.licenses.gpl2Only;
+  };
+})
--- packages/classpath-99/package.nix
+++ packages/classpath-99/package.nix
@@ -2,43 +2,50 @@
   fetchurl,
   findutils,
   glib,
-  jikes,
   lib,
   pkg-config,
   stdenv,
   zip,
-  callPackage,
   ecj,
   jamvm-14,
 }:
-
-let
-  classpath99 =
-    (callPackage ../classpath-93/base.nix {
-      hash = "sha256-+Skpf4rpthOhoWfiMVZoYYkyYGUdkTrZtsEZM4lf7Mg=";
-    }).overrideAttrs
-      (
-        final: prev: {
-          withEcj = ecj;
-          patches = [
-            ./configure.patch
-            ./classpath-99.patch
-          ];
-          nativeBuildInputs = prev.nativeBuildInputs ++ [
-            jamvm-14
-          ];
-          configureFlags = prev.configureFlags ++ [
-            # TODO: this is used for com.sun.tools.javac.Main, but will only load the jar itself, not the rest of the classpath
-            "--with-ecj-jar=${ecj}/lib/org.eclipse.jdt.core.jar"
-            # build with zip TODO: use previous classpath gjar?
-            "--without-jar"
-            # TODO: fix some stringop truncation errors
-            "--disable-Werror"
-            "--disable-gjdoc"
-          ];
-          version = "0.99";
-          ECJ_JVM_OPTS = "-Xmx3000m -Xss32m";
-        }
-      );
-in
-classpath99
+stdenv.mkDerivation (finalAttrs: {
+  pname = "classpath";
+  version = "0.99";
+  src = fetchurl {
+    url = "mirror://gnu/classpath/classpath-${finalAttrs.version}.tar.gz";
+    hash = "sha256-+Skpf4rpthOhoWfiMVZoYYkyYGUdkTrZtsEZM4lf7Mg=";
+  };
+  buildInputs = [
+    glib
+  ];
+  nativeBuildInputs = [
+    pkg-config
+    findutils
+    zip
+    jamvm-14
+    ecj
+  ];
+  configureFlags = [
+    "--disable-plugin"
+    "--disable-gtk-peer"
+    "--enable-default-preferences-peer=memory"
+    "--disable-gconf-peer"
+    # "--disable-examples"
+    # "--enable-Werror"
+    # TODO: this is used for com.sun.tools.javac.Main, but will only load the jar itself, not the rest of the classpath
+    "--with-ecj-jar=${ecj}/lib/org.eclipse.jdt.core.jar"
+    # build with zip TODO: use previous classpath gjar?
+    "--without-jar"
+    # TODO: fix some stringop truncation errors
+    "--disable-Werror"
+    "--disable-gjdoc"
+  ];
+  patches = [
+    ./classpath-99.patch
+  ];
+  ECJ_JVM_OPTS = "-Xmx3000m -Xss32m";
+  meta = {
+    license = lib.licenses.gpl2Only;
+  };
+})
